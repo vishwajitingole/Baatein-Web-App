347fc0e4aebc7b60252b98c834415c8962094eaa
Vishwajeet Vikas Ingole <72921903+vishwajit022@users.noreply.github.com>
2023-09-19 01:57:14 +0530

    .

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -1,59 +1,64 @@
-import React from 'react';
 import MyMessage from './MyMessage';
 import TheirMessage from './TheirMessage';
 import MessageForm from './MessageForm';
 
-function ChatFeed(props) {
-    console.log(props);
-    const { chats, activeChat, userName, messages } = props;
+const ChatFeed = (props) => {
+  const { chats, activeChat, userName, messages } = props;
 
-    // Only if the chat is present this code will run
-    const chat = chats && chats[activeChat];
+  const chat = chats && chats[activeChat];
 
-    const renderMessages = () => {
-        // Every message that we store will have a unique key
-        const keys = Object.keys(messages);
+  const renderReadReceipts = (message, isMyMessage) => chat.people.map((person, index) => person.last_read === message.id && (
+    <div
+      key={`read_${index}`}
+      className="read-receipt"
+      style={{
+        float: isMyMessage ? 'right' : 'left',
+        backgroundImage: person.person.avatar && `url(${person.person.avatar})`,
+      }}
+    />
+  ));
 
-        return keys.map((key, index) => {
-            const message = messages[key];
-            const lastMessageKey = index === 0 ? null : keys[index - 1];
-            const isMyMessage = userName === message.sender.username; // Use '===' instead of '==' for comparison.
+  const renderMessages = () => {
+    const keys = Object.keys(messages);
 
-            return (
-                <div className="" key={`msg_${index}`} style={{ width: '100%' }}>
-                    <div className="message-block">
-                        {isMyMessage ? (
-                            <MyMessage message={message} />
-                        ) : (
-                            <TheirMessage message={message} lastMessage={messages[lastMessageKey]} />
-                        )}
-                    </div>
-                    <div
-                        className="read-reciepts"
-                        style={{
-                            marginRight: isMyMessage ? '0px' : '18px', // Corrected the '>' to '<' here
-                            marginLeft: isMyMessage ? '68px' : '0px', // Corrected the '>' to '<' here
-                        }}
-                    ></div>
-                </div>
-            );
-        });
-    };
+    return keys.map((key, index) => {
+      const message = messages[key];
+      const lastMessageKey = index === 0 ? null : keys[index - 1];
+      const isMyMessage = userName === message.sender.username;
 
-    return <div className='chat-feed' >
-        <div className="chat-tittle-container">
-            <div className="chat-title">{chat?.title//If there exist any chat it will show the title of the chart
-            }</div>
-            <div className="chat-subtitle">
-
-            </div>
-            {renderMessages()}
-            <div style={{height:'100px'}} />
-            <div className="message-form-container">
-                <MessageForm{...props} chatId={activeChat} />
-            </div>
+      return (
+        <div key={`msg_${index}`} style={{ width: '100%' }}>
+          <div className="message-block">
+            {isMyMessage
+              ? <MyMessage message={message} />
+              : <TheirMessage message={message} lastMessage={messages[lastMessageKey]} />}
+          </div>
+          <div className="read-receipts" style={{ marginRight: isMyMessage ? '18px' : '0px', marginLeft: isMyMessage ? '0px' : '68px' }}>
+            {renderReadReceipts(message, isMyMessage)}
+          </div>
         </div>
-        </div>;
-}
+      );
+    });
+  };
+
+  if (!chat) return <div />;
+
+  return (
+    <div className="chat-feed">
+      <div className="chat-title-container">
+        <div className="chat-title">{chat?.title}</div>
+        <div className="chat-subtitle">
+          {chat.people.map((person) => ` ${person.person.username}`)}
+        </div>
+      </div>
+      {renderMessages()}
+      <div style={{ height: '100px' }} />
+      <div className="message-form-container">
+        <MessageForm {...props} chatId={activeChat} />
+      </div>
+    </div>
+  );
+};
 
 export default ChatFeed;
+
--- src/components/TheirMessage.jsx
+++ src/components/TheirMessage.jsx
@@ -1,35 +1,30 @@
-import React from 'react';
-
-function TheirMessage({ lastMessage, message }) {
-  const isFirstMessageByUser =
-    !lastMessage || lastMessage.sender.username !== message.sender.username;
+const TheirMessage = ({ lastMessage, message }) => {
+  const isFirstMessageByUser = !lastMessage || lastMessage.sender.username !== message.sender.username;
 
   return (
     <div className="message-row">
       {isFirstMessageByUser && (
         <div
           className="message-avatar"
-          style={{
-            backgroundImage: message.sender && `url(${message.sender.avatar})`,
-          }}
+          style={{ backgroundImage: message.sender && `url(${message.sender.avatar})` }}
         />
       )}
-
-      {message.attachment && message.attachments.length > 0 && (
-        <img
-          src={message.attachment}
-          alt="Message Attachment"
-          className="message-image"
-        />
-      )}
-
-      {message.text && (
-        <div className="message" style={{ float: 'left', backgroundColor: '#CABCDC' }}>
-          {message.text}
-        </div>
-      )}
+      {message.attachments && message.attachments.length > 0
+        ? (
+          <img
+            src={message.attachments[0].file}
+            alt="message-attachment"
+            className="message-image"
+            style={{ marginLeft: isFirstMessageByUser ? '4px' : '48px' }}
+          />
+        )
+        : (
+          <div className="message" style={{ float: 'left', backgroundColor: '#CABCDC', marginLeft: isFirstMessageByUser ? '4px' : '48px' }}>
+            {message.text}
+          </div>
+        )}
     </div>
   );
-}
+};
 
 export default TheirMessage;
