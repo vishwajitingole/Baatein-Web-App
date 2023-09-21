d59633ac030a4ea83861b99a6d1ce6e140c801a1
Vishwajeet Vikas Ingole <72921903+vishwajit022@users.noreply.github.com>
2023-09-22 02:39:42 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-
-import {ChatEngine} from "react-chat-engine";
-import ChatFeed from "./components/ChatFeed";
-
-
+import { ChatEngine } from 'react-chat-engine';
 import './App.css';
+import ChatFeed from './Components/ChatFeed';
 
 function App() {
   return (
     <div className="App">
-     <ChatEngine 
-     height="100vh"
-     projectID="cb0320f6-e39c-4061-a18f-779c41bf5d3e"
-     userName="Vishwajit"
-     userSecret="Vishwajit"
-     renderChatFeed={
-    (chatAppProps)=><ChatFeed{...chatAppProps}/>
-    }
-    onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+      
+        <ChatEngine
+        height="100vh"
+        projectID="d0b1fd21-68ea-42dd-b1cb-c20269aeb8f6"
+        userName="Vishwajit"
+        userSecret="Vishwajit"
+        renderChatFeed={(chatAppProps)=><ChatFeed  {...chatAppProps} />}
+        onNewMessage={()=>new Audio('https://chat-engine-assests.s3.amazonaws.com/click.mp3').play()}
+        />
 
-     />
+      
     </div>
-  );
-}
+  )
+};
 
 export default App;
