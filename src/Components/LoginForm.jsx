1eb59676d3d5e32b7400790e70497e50ec2ab0b2
Vishwajeet Vikas Ingole <72921903+vishwajit022@users.noreply.github.com>
2023-09-22 05:33:09 +0530

    go

--- src/Components/LoginForm.jsx
+++ src/Components/LoginForm.jsx
@@ -29,17 +29,20 @@ const Modal = () => {
   return (
     <div className="wrapper">
       <div className="form">
-        <h1 className="title">Chat Application</h1>
+        <h1 className="title">Baatein</h1>
+        
         <form onSubmit={handleSubmit}>
           <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} className="input" placeholder="Username" required />
           <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="input" placeholder="Password" required />
           <div align="center">
             <button type="submit" className="button">
-              <span>Start chatting</span>
+              <span>Login</span>
             </button>
           </div>
         </form>
         <h1>{error}</h1>
+        <footer className='title'>Created by Vishwajit</footer>
+
       </div>
     </div>
 
